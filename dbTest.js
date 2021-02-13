bf887b2f5b7e0a39ed5d282da5ada6f5515d17d2
James Sinkler <jamessinkler713@gmail.com>
2021-02-12 18:55:23 -0800

    dbTest: working with basic create, and async create with association

--- dbTest.js
+++ dbTest.js
@@ -48,39 +48,38 @@ const db = require('./models')
 
 
 
-// const postRouteEx = async() => {
-//   try{
-//     console.log("in the post route")
-//     // pretendng we have a req.body like we will
-//     const req = {}
-//     req.body= {
-//       name: 'hey',
-//       githubLink: 'aslkdfj.com',
-//       deployedLink: 'www.google.com',
-//       description: 'project description here',
-//       category: 'category name here'
-//     }
-//     const project = await db.project.create({
-//       name: req.body.name,
-//       githubLink: req.body.githubLink,
-//       deployLink: req.body.deployedLink,
-//       description: req.body.description,
-//     })
-//     const [category, wasCreated] = await db.category.findOrCreate({
-//       where:{
-//         name: req.body.category
-//       },
-//       include: [db.project]
-//     })
-//       await category.addProject(project)
-//       
-//       console.log(category)
-//       console.log(category.projects)
-//       // not a real route res.redirect('/project/catshow')
-//   } catch(e){
-//     console.log(e.message)
-//   }
-// }
+const postRouteEx = async() => {
+  try{
+    console.log("in the post route")
+    // pretendng we have a req.body like we will
+    const req = {}
+    req.body= {
+      name: 'THIS REPO',
+      githubLink: 'https://git.generalassemb.ly/SEIR-111/express-project-organizer',
+      deployedLink: 'https://git.generalassemb.ly/SEIR-111/express-project-organizer',
+      description: 'It is actually this project',
+      category: 'many to many'
+    }
+    const project = await db.project.create({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployLink: req.body.deployedLink,
+      description: req.body.description,
+    })
+    const [category, wasCreated] = await db.category.findOrCreate({
+      where:{
+        name: req.body.category
+      },
+      include: [db.project]
+    })
+      let resultJoinTableInsertion = await category.addProject(project)
+      console.log('result')
+    console.log(resultJoinTableInsertion)
+  } catch(e){
+    console.log(e.message)
+    // not a real route res.redirect('/project/catshow')
+  }
+}
 // call the function
-// postRouteEx()
+postRouteEx()
 
