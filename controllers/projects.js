34034680bde000e858eb76c12456c5c62b9825e2
James Sinkler <jamessinkler713@gmail.com>
2021-02-12 19:07:21 -0800

    projects post route: find or create category and make association to project created

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,19 +3,24 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then((project) => {
+router.post('/', async(req, res) => {
+  try {
+    const newProject = await db.project.create({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployLink: req.body.deployedLink,
+      description: req.body.description
+    })
+    const [category, wasCreated] = await db.project.findOrCreate({
+      where: {
+        name: req.body.category
+      }
+    })
     res.redirect('/')
-  })
-  .catch((error) => {
+  }
+  catch(error) {
     res.status(400).render('main/404')
-  })
+  }
 })
 
 // GET /projects/new - display form for creating a new project
